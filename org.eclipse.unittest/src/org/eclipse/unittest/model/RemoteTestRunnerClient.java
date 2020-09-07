299eeb72394df967c356a8b513ae3228096106d0
Victor Rubezhny <vrubezhny@redhat.com>
2020-09-07 23:23:00 +0200

    Documanting API
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- org.eclipse.unittest/src/org/eclipse/unittest/model/RemoteTestRunnerClient.java
+++ org.eclipse.unittest/src/org/eclipse/unittest/model/RemoteTestRunnerClient.java
@@ -50,23 +50,6 @@ public abstract class RemoteTestRunnerClient extends TestRunnerClient {
 	}
 
 	/**
-	 * The failed trace that is currently reported from the RemoteTestRunner
-	 */
-//	protected final StringBuffer fFailedTrace = new StringBuffer();
-	/**
-	 * The expected test result
-	 */
-//	protected final StringBuffer fExpectedResult = new StringBuffer();
-	/**
-	 * The actual test result
-	 */
-//	protected final StringBuffer fActualResult = new StringBuffer();
-	/**
-	 * The failed trace of a reran test
-	 */
-//	protected final StringBuffer fFailedRerunTrace = new StringBuffer();
-
-	/**
 	 * The server socket
 	 */
 	private ServerSocket fServerSocket;
@@ -81,18 +64,6 @@ public abstract class RemoteTestRunnerClient extends TestRunnerClient {
 	 * The protocol version
 	 */
 	protected String fVersion;
-	/**
-	 * The failed test that is currently reported from the RemoteTestRunner
-	 */
-//	protected String fFailedTest;
-	/**
-	 * The Id of the failed test
-	 */
-//	protected String fFailedTestId;
-	/**
-	 * The kind of failure of the test that is currently reported as failed
-	 */
-//	protected int fFailureKind;
 
 	@SuppressWarnings("hiding")
 	protected boolean fDebug = false;
@@ -204,113 +175,4 @@ public abstract class RemoteTestRunnerClient extends TestRunnerClient {
 			return null;
 		return buf.toString();
 	}
-	/*
-	 * protected void notifyTestReran(String testId, String className, String
-	 * testName, String status) { int statusCode= ITestRunListener3.STATUS_OK; if
-	 * (status.equals("FAILURE")) //$NON-NLS-1$ statusCode=
-	 * ITestRunListener3.STATUS_FAILURE; else if (status.equals("ERROR"))
-	 * //$NON-NLS-1$ statusCode= ITestRunListener3.STATUS_ERROR;
-	 *
-	 * String trace= ""; //$NON-NLS-1$ if (statusCode !=
-	 * ITestRunListener3.STATUS_OK) trace = fFailedRerunTrace.toString(); //
-	 * assumption a rerun trace was sent before notifyTestReran(testId, className,
-	 * testName, statusCode, trace); }
-	 *
-	 * abstract protected void extractFailure(String arg, int status);
-	 */
-	/**
-	 * @param arg test name
-	 * @return an array with two elements. The first one is the testId, the second
-	 *         one the testName.
-	 */
-//	abstract protected String[] extractTestId(String arg);
-
-//	abstract protected boolean hasTestId();
-
-	/*
-	 * protected void notifyTestReran(final String testId, final String className,
-	 * final String testName, final int statusCode, final String trace) { for
-	 * (ITestRunListener3 listener : fListeners) { SafeRunner.run(new
-	 * ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testReran(testId, className, testName,
-	 * statusCode, trace, nullifyEmpty(fExpectedResult),
-	 * nullifyEmpty(fActualResult)); } }); } }
-	 */
-	/*
-	 * protected void notifyTestTreeEntry(final String treeEntry) { for
-	 * (ITestRunListener3 listener : fListeners) { if (!hasTestId())
-	 * listener.testTreeEntry(fakeTestId(treeEntry)); else
-	 * listener.testTreeEntry(treeEntry); } }
-	 *
-	 * private String fakeTestId(String treeEntry) { // extract the test name and
-	 * add it as the testId int index0= treeEntry.indexOf(','); String testName=
-	 * treeEntry.substring(0, index0).trim(); return testName+","+treeEntry;
-	 * //$NON-NLS-1$ }
-	 *
-	 * protected void notifyTestRunStopped(final long elapsedTime) { if
-	 * (UnitTestPlugin.isStopped()) return; for (ITestRunListener3 listener :
-	 * fListeners) { SafeRunner.run(new ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testRunStopped(elapsedTime); } }); } }
-	 *
-	 * protected void testRunEnded(final long elapsedTime) { if
-	 * (UnitTestPlugin.isStopped()) return; for (ITestRunListener3 listener :
-	 * fListeners) { SafeRunner.run(new ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testRunEnded(elapsedTime); } }); } }
-	 *
-	 * protected void notifyTestEnded(final String test) { if
-	 * (UnitTestPlugin.isStopped()) return; for (ITestRunListener3 listener :
-	 * fListeners) { SafeRunner.run(new ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { String s[]= extractTestId(test);
-	 * listener.testEnded(s[0], s[1]); } }); } }
-	 *
-	 * protected void notifyTestStarted(final String test) { if
-	 * (UnitTestPlugin.isStopped()) return; for (ITestRunListener3 listener :
-	 * fListeners) { SafeRunner.run(new ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { String s[]= extractTestId(test);
-	 * listener.testStarted(s[0], s[1]); } }); } }
-	 *
-	 * protected void notifyTestRunStarted(final int count) { if
-	 * (UnitTestPlugin.isStopped()) return; for (ITestRunListener3 listener :
-	 * fListeners) { SafeRunner.run(new ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testRunStarted(count); } }); } }
-	 *
-	 * protected void notifyTestFailed() { if (UnitTestPlugin.isStopped()) return;
-	 * for (ITestRunListener3 listener : fListeners) { SafeRunner.run(new
-	 * ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testFailed(fFailureKind,
-	 * fFailedTestId, fFailedTest, fFailedTrace.toString(),
-	 * nullifyEmpty(fExpectedResult), nullifyEmpty(fActualResult)); } }); } }
-	 */
-	/**
-	 * Returns a comparison result from the given buffer. Removes the terminating
-	 * line delimiter.
-	 *
-	 * @param buf the comparison result
-	 * @return the result or <code>null</code> if empty
-	 */
-	/*
-	 * private static String nullifyEmpty(StringBuffer buf) { int length=
-	 * buf.length(); if (length == 0) return null;
-	 *
-	 * char last= buf.charAt(length - 1); if (last == '\n') { if (length > 1 &&
-	 * buf.charAt(length - 2) == '\r') return buf.substring(0, length - 2); else
-	 * return buf.substring(0, length - 1); } else if (last == '\r') { return
-	 * buf.substring(0, length - 1); } return buf.toString(); }
-	 *
-	 * protected void notifyTestRunTerminated() { if (UnitTestPlugin.isStopped())
-	 * return; for (ITestRunListener3 listener : fListeners) { SafeRunner.run(new
-	 * ListenerSafeRunnable() {
-	 *
-	 * @Override public void run() { listener.testRunTerminated(); } }); } }
-	 *
-	 * @Override abstract public void rerunTest(String testId, String className,
-	 * String testName);
-	 */
 }
