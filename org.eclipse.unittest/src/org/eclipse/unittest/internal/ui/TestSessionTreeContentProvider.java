2959f6b8bc80cac58f26582c319b9e765e5633fd
Victor Rubezhny <vrubezhny@redhat.com>
2020-10-17 01:14:42 +0200

    Test elements are alphabetically sortedon the tree
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- org.eclipse.unittest/src/org/eclipse/unittest/internal/ui/TestSessionTreeContentProvider.java
+++ org.eclipse.unittest/src/org/eclipse/unittest/internal/ui/TestSessionTreeContentProvider.java
@@ -14,9 +14,14 @@
 
 package org.eclipse.unittest.internal.ui;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import org.eclipse.unittest.internal.model.TestElement;
 import org.eclipse.unittest.internal.model.TestRunSession;
 import org.eclipse.unittest.internal.model.TestSuiteElement;
+import org.eclipse.unittest.model.ITestElement;
+import org.eclipse.unittest.model.ITestSuiteElement;
 
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
@@ -33,7 +38,18 @@ public class TestSessionTreeContentProvider implements ITreeContentProvider {
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof TestSuiteElement) {
-			return ((TestSuiteElement) parentElement).getChildren().toArray(Object[]::new);
+			Set<ITestElement> sortedChildren = new TreeSet<>((element1, element2) -> {
+				// Show test suites on top of test messages
+				int weight1 = (element1 instanceof ITestSuiteElement) ? 0 : 1;
+				int weight2 = (element2 instanceof ITestSuiteElement) ? 0 : 1;
+				if (weight1 != weight2) {
+					return weight1 - weight2;
+				}
+				// Compare by element names
+				return element1.getTestName().compareTo(element2.getTestName());
+			});
+			sortedChildren.addAll(((TestSuiteElement) parentElement).getChildren());
+			return sortedChildren.toArray(Object[]::new);
 		} else {
 			return NO_CHILDREN;
 		}
